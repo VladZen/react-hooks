81faa2fbdf70d2129a4ce78d5788cf806b903d78
Vladislav Drannikov <vladislav.drannikov@justwebit.ru>
2021-03-29 14:09:25 +0300

    constate has been added

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,9 @@ module.exports = {
       '@images': path.resolve(__dirname, 'src/assets/img/'),
       '@components': path.resolve(__dirname, 'src/components/'),
       '@utils': path.resolve(__dirname, 'src/utils/'),
-      '@pages': path.resolve(__dirname, 'src/pages/')
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@types': path.resolve(__dirname, 'src/types/'),
+      '@contexts': path.resolve(__dirname, 'src/contexts/')
     }
   }
 }
